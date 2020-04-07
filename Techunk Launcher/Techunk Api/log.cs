adb08905b1a5fbe5450eb8141d5e68c95b71eb9c
Kaiserdj <gonferfr@gmail.com>
2020-04-07 19:44:19 +0200

    Inicio de implementación de log

---
+++ Techunk Launcher/Techunk Api/log.cs
@@ -0,0 +1,28 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Reflection;
+using System.Xml;
+
+namespace Techunk_Api
+{
+    public class log
+    {
+        public void main_log()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(Properties.Resources.log4net);
+            XmlElement config = doc.DocumentElement;
+            XmlConfigurator.Configure(LogManager.GetRepository(Assembly.GetEntryAssembly()), config);
+
+            Console.WriteLine("Hello world!");
+
+            // Log some things
+            log_.Info("Hello logging world!");
+            log_.Error("Error!");
+            log_.Warn("Warn!");
+        }
+
+        public static readonly ILog log_ = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+    }
+}
