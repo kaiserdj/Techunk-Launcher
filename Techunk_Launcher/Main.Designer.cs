79de18e235b29603038a4cbcf5d5d210cebb7c8f
Kaiserdj <gonferfr@gmail.com>
2020-04-09 02:10:14 +0200

    Actulización parte 2
    AlphaBs/CmlLib.Core@c2c6ca0

---
+++ Techunk_Launcher/Main.Designer.cs
@@ -0,0 +1,409 @@
+﻿namespace Techunk_Launcher
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Btn_loginOption = new System.Windows.Forms.Button();
+            this.Btn_Launch = new System.Windows.Forms.Button();
+            this.Cb_Version = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Btn_Login = new System.Windows.Forms.Button();
+            this.Btn_Apply = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Txt_Pass = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Txt_User = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Txt_Path = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Txt_Java = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Txt_Ram = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Txt_JavaArgs = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Txt_ScHt = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.Txt_ScWd = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.Txt_LauncherName = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.Txt_ServerIp = new System.Windows.Forms.TextBox();
+            this.Lv_Status = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Btn_loginOption);
+            this.groupBox1.Controls.Add(this.Btn_Launch);
+            this.groupBox1.Controls.Add(this.Cb_Version);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.Btn_Login);
+            this.groupBox1.Controls.Add(this.Btn_Apply);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.Txt_Pass);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.Txt_User);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.Txt_Path);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.Txt_Java);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(326, 189);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Launcher";
+            // 
+            // Btn_loginOption
+            // 
+            this.Btn_loginOption.Location = new System.Drawing.Point(206, 0);
+            this.Btn_loginOption.Name = "Btn_loginOption";
+            this.Btn_loginOption.Size = new System.Drawing.Size(112, 25);
+            this.Btn_loginOption.TabIndex = 15;
+            this.Btn_loginOption.Text = "Opciones de sesión";
+            this.Btn_loginOption.UseVisualStyleBackColor = true;
+            this.Btn_loginOption.Click += new System.EventHandler(this.Btn_loginOption_Click);
+            // 
+            // Btn_Launch
+            // 
+            this.Btn_Launch.Location = new System.Drawing.Point(242, 133);
+            this.Btn_Launch.Name = "Btn_Launch";
+            this.Btn_Launch.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Launch.TabIndex = 11;
+            this.Btn_Launch.Text = "Arrancar";
+            this.Btn_Launch.UseVisualStyleBackColor = true;
+            this.Btn_Launch.Click += new System.EventHandler(this.Btn_Launch_Click);
+            // 
+            // Cb_Version
+            // 
+            this.Cb_Version.FormattingEnabled = true;
+            this.Cb_Version.Location = new System.Drawing.Point(89, 135);
+            this.Cb_Version.Name = "Cb_Version";
+            this.Cb_Version.Size = new System.Drawing.Size(147, 21);
+            this.Cb_Version.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(36, 138);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Versión:";
+            // 
+            // Btn_Login
+            // 
+            this.Btn_Login.Location = new System.Drawing.Point(243, 85);
+            this.Btn_Login.Name = "Btn_Login";
+            this.Btn_Login.Size = new System.Drawing.Size(75, 43);
+            this.Btn_Login.TabIndex = 8;
+            this.Btn_Login.Text = "Iniciar sesión";
+            this.Btn_Login.UseVisualStyleBackColor = true;
+            this.Btn_Login.Click += new System.EventHandler(this.Btn_Login_Click);
+            // 
+            // Btn_Apply
+            // 
+            this.Btn_Apply.Location = new System.Drawing.Point(242, 28);
+            this.Btn_Apply.Name = "Btn_Apply";
+            this.Btn_Apply.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Apply.TabIndex = 1;
+            this.Btn_Apply.Text = "Aplicar";
+            this.Btn_Apply.UseVisualStyleBackColor = true;
+            this.Btn_Apply.Click += new System.EventHandler(this.Btn_Apply_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Contraseña:";
+            // 
+            // Txt_Pass
+            // 
+            this.Txt_Pass.Location = new System.Drawing.Point(88, 108);
+            this.Txt_Pass.Name = "Txt_Pass";
+            this.Txt_Pass.Size = new System.Drawing.Size(148, 20);
+            this.Txt_Pass.TabIndex = 6;
+            this.Txt_Pass.UseSystemPasswordChar = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Email/Usuario:";
+            // 
+            // Txt_User
+            // 
+            this.Txt_User.Location = new System.Drawing.Point(88, 82);
+            this.Txt_User.Name = "Txt_User";
+            this.Txt_User.Size = new System.Drawing.Size(148, 20);
+            this.Txt_User.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Path:";
+            // 
+            // Txt_Path
+            // 
+            this.Txt_Path.Location = new System.Drawing.Point(88, 30);
+            this.Txt_Path.Name = "Txt_Path";
+            this.Txt_Path.Size = new System.Drawing.Size(148, 20);
+            this.Txt_Path.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Java:";
+            // 
+            // Txt_Java
+            // 
+            this.Txt_Java.Location = new System.Drawing.Point(88, 56);
+            this.Txt_Java.Name = "Txt_Java";
+            this.Txt_Java.Size = new System.Drawing.Size(148, 20);
+            this.Txt_Java.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.Txt_Ram);
+            this.groupBox2.Controls.Add(this.label11);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.Txt_JavaArgs);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.Txt_ScHt);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.Txt_ScWd);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.Txt_LauncherName);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.Txt_ServerIp);
+            this.groupBox2.Location = new System.Drawing.Point(386, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(326, 224);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ajustes";
+            // 
+            // Txt_Ram
+            // 
+            this.Txt_Ram.Location = new System.Drawing.Point(103, 134);
+            this.Txt_Ram.Name = "Txt_Ram";
+            this.Txt_Ram.Size = new System.Drawing.Size(200, 20);
+            this.Txt_Ram.TabIndex = 11;
+            this.Txt_Ram.Text = "1024";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(44, 138);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(56, 13);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "Ram (Mb):";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(31, 173);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 26);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Ajustes de\r\nlanzamiento:";
+            // 
+            // Txt_JavaArgs
+            // 
+            this.Txt_JavaArgs.Location = new System.Drawing.Point(103, 160);
+            this.Txt_JavaArgs.Multiline = true;
+            this.Txt_JavaArgs.Name = "Txt_JavaArgs";
+            this.Txt_JavaArgs.Size = new System.Drawing.Size(200, 49);
+            this.Txt_JavaArgs.TabIndex = 8;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(27, 111);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(70, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Alto ventana:";
+            // 
+            // Txt_ScHt
+            // 
+            this.Txt_ScHt.Location = new System.Drawing.Point(103, 108);
+            this.Txt_ScHt.Name = "Txt_ScHt";
+            this.Txt_ScHt.Size = new System.Drawing.Size(200, 20);
+            this.Txt_ScHt.TabIndex = 6;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(14, 85);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(83, 13);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "Ancho ventana:";
+            // 
+            // Txt_ScWd
+            // 
+            this.Txt_ScWd.Location = new System.Drawing.Point(103, 82);
+            this.Txt_ScWd.Name = "Txt_ScWd";
+            this.Txt_ScWd.Size = new System.Drawing.Size(200, 20);
+            this.Txt_ScWd.TabIndex = 4;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(11, 33);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(86, 13);
+            this.label9.TabIndex = 3;
+            this.label9.Text = "Launcher Name:";
+            // 
+            // Txt_LauncherName
+            // 
+            this.Txt_LauncherName.Location = new System.Drawing.Point(103, 30);
+            this.Txt_LauncherName.Name = "Txt_LauncherName";
+            this.Txt_LauncherName.Size = new System.Drawing.Size(200, 20);
+            this.Txt_LauncherName.TabIndex = 2;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(43, 59);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(54, 13);
+            this.label10.TabIndex = 1;
+            this.label10.Text = "Server IP:";
+            // 
+            // Txt_ServerIp
+            // 
+            this.Txt_ServerIp.Location = new System.Drawing.Point(103, 56);
+            this.Txt_ServerIp.Name = "Txt_ServerIp";
+            this.Txt_ServerIp.Size = new System.Drawing.Size(200, 20);
+            this.Txt_ServerIp.TabIndex = 0;
+            // 
+            // Lv_Status
+            // 
+            this.Lv_Status.AutoSize = true;
+            this.Lv_Status.Location = new System.Drawing.Point(41, 223);
+            this.Lv_Status.Name = "Lv_Status";
+            this.Lv_Status.Size = new System.Drawing.Size(56, 13);
+            this.Lv_Status.TabIndex = 12;
+            this.Lv_Status.Text = "Preparado";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 250);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(700, 23);
+            this.progressBar1.TabIndex = 13;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(12, 279);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(700, 23);
+            this.progressBar2.TabIndex = 14;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(733, 327);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.Lv_Status);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Main";
+            this.Text = "Launcher Techunk";
+            this.Shown += new System.EventHandler(this.Main_Shown);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Txt_Java;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Txt_Path;
+        private System.Windows.Forms.Button Btn_Login;
+        private System.Windows.Forms.Button Btn_Apply;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox Txt_Pass;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Txt_User;
+        private System.Windows.Forms.ComboBox Cb_Version;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button Btn_Launch;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox Txt_Ram;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox Txt_JavaArgs;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox Txt_ScHt;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox Txt_ScWd;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox Txt_LauncherName;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox Txt_ServerIp;
+        private System.Windows.Forms.Label Lv_Status;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Button Btn_loginOption;
+    }
+}
---
+++ Techunk_Launcher/Main.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using Techunk_Api;
+using Techunk_Api.Core;
+using System.ComponentModel;
+
+namespace Techunk_Launcher
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        Techunk Launcher;
+        MSession Session;
+        GameConsole GameConsole;
+        bool allowOffline = true;
+
+        private void Main_Shown(object sender, EventArgs e)
+        {
+            // Initialize launcher
+            Txt_Path.Text = Minecraft.GetOSDefaultPath();
+
+            var th = new Thread(new ThreadStart(delegate
+            {
+                InitializeLauncher();
+
+                // Try auto login
+
+                var login = new MLogin();
+                MSession result = login.TryAutoLogin();
+
+                if (result.Result != MLoginResult.Success)
+                    return;
+
+                MessageBox.Show("¡Auto inicio de sesión completado!\n\nUsuario: " + result.Username);
+                Session = result;
+
+                Invoke((MethodInvoker)delegate
+                {
+                    Btn_Login.Enabled = false;
+                    Btn_Login.Text = "Auto inicio\nCompletado";
+                    Txt_User.Text = Session.Username;
+                    Txt_Pass.Text = "#########";
+                });
+            }));
+            th.Start();
+        }
+
+        private void Btn_Apply_Click(object sender, EventArgs e)
+        {
+            InitializeLauncher();
+        }
+
+        private void InitializeLauncher()
+        {
+            Launcher = new Techunk(Txt_Path.Text);
+            Launcher.FileChanged += Launcher_FileChanged;
+            Launcher.ProgressChanged += Launcher_ProgressChanged;
+            var versions = Launcher.UpdateProfileInfos();
+
+            Invoke(new Action(() =>
+            {
+                Cb_Version.Items.Clear();
+                foreach (var item in versions)
+                {
+                    Cb_Version.Items.Add(item.Name);
+                }
+            }));
+        }
+
+        private void Btn_Login_Click(object sender, EventArgs e)
+        {
+            // Login
+            if (Txt_User.Text == "")
+            {
+                MessageBox.Show("El campo usuario no puede estar vacio");
+                return;
+            }
+
+            Btn_Login.Enabled = false;
+            if (Txt_Pass.Text == "")
+            {
+                if (allowOffline)
+                {
+                    var login = new MLogin();
+                    // session = MSession.GetOfflineSession(Txt_User.Text);
+                    Session = login.offline(Txt_User.Text);
+                    MessageBox.Show("Inicio pirata completado : " + Session.Username);
+                }
+                else
+                {
+                    MessageBox.Show("La contraseña no puede estar vacia");
+                    Btn_Login.Enabled = true;
+                    return;
+                }
+            }
+            else
+            {
+                var th = new Thread(new ThreadStart(delegate
+                {
+                    var login = new MLogin();
+                    var result = login.Authenticate(Txt_User.Text, Txt_Pass.Text);
+                    if (result.Result == MLoginResult.Success)
+                    {
+                        MessageBox.Show("Inicio completado : " + result.Username);
+                        Session = result;
+                    }
+                    else
+                    {
+                        MessageBox.Show(result.Result.ToString() + "\n" + result.Message);
+                        Invoke((MethodInvoker)delegate { Btn_Login.Enabled = true; });
+                    }
+                }));
+                th.Start();
+            }
+        }
+
+        private void Btn_Launch_Click(object sender, EventArgs e)
+        {
+            // Launch
+
+            if (Session == null)
+            {
+                MessageBox.Show("Inicie sesión primero");
+                return;
+            }
+
+
+            if (Cb_Version.Text == "")
+            {
+                MessageBox.Show("Porfavor eliga una versión");
+                return;
+            }
+            groupBox1.Enabled = false;
+            groupBox2.Enabled = false;
+
+            var versionname = Cb_Version.Text;
+            var launchOption = new MLaunchOption()
+            {
+                JavaPath = Txt_Java.Text,
+                MaximumRamMb = int.Parse(Txt_Ram.Text),
+                Session = this.Session,
+
+                LauncherName = Txt_LauncherName.Text,
+                ServerIp = Txt_ServerIp.Text,
+                CustomJavaParameter = Txt_JavaArgs.Text
+            };
+
+            if (Txt_ScWd.Text != "" && Txt_ScHt.Text != "")
+            {
+                launchOption.ScreenHeight = int.Parse(Txt_ScHt.Text);
+                launchOption.ScreenWidth = int.Parse(Txt_ScWd.Text);
+            }
+
+            var th = new Thread(() =>
+            {
+                var process = Launcher.CreateProcess(versionname, launchOption);
+                StartProcess(process);
+
+                Invoke(new Action(() =>
+                {
+                    if (GameConsole != null)
+                        GameConsole.Close();
+
+                    GameConsole = new GameConsole();
+                    GameConsole.Show();
+
+                    groupBox1.Enabled = true;
+                    groupBox2.Enabled = true;
+                }));
+            });
+            th.Start();
+        }
+
+        private void Launcher_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            Invoke(new Action(() =>
+            {
+                progressBar2.Value = e.ProgressPercentage;
+            }));
+        }
+
+        private void Launcher_FileChanged(DownloadFileChangedEventArgs e)
+        {
+            Invoke(new Action(() =>
+            {
+                progressBar1.Maximum = e.TotalFileCount;
+                progressBar1.Value = e.ProgressedFileCount;
+                Lv_Status.Text = e.FileKind.ToString() + " : " + e.FileName;
+            }));
+        }
+
+        private void StartProcess(Process process)
+        {
+            File.WriteAllText("launcher.txt", process.StartInfo.Arguments);
+
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.EnableRaisingEvents = true;
+            process.ErrorDataReceived += Process_ErrorDataReceived;
+            process.OutputDataReceived += Process_OutputDataReceived;
+
+            process.Start();
+            process.BeginErrorReadLine();
+            process.BeginOutputReadLine();
+        }
+
+        private void Process_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            output(e.Data);
+        }
+
+        private void Process_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            output(e.Data);
+        }
+
+        void output(string msg)
+        {
+            Invoke(new Action(() =>
+            {
+                if (GameConsole != null && !GameConsole.IsDisposed)
+                    GameConsole.AddLog(msg);
+            }));
+        }
+
+        private void Btn_loginOption_Click(object sender, EventArgs e)
+        {
+            var form3 = new Logout_and_Cache();
+            form3.Show();
+        }
+    }
+}
